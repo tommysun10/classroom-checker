74618c91008e42cc471371246831d6de3f4641c1
Tommy Sun <tommy.sun@libertymutual.com>
2019-09-10 15:10:41 -0400

    Added logic to parse json

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,35 +1,42 @@
-import React, { FC, Fragment, useEffect, useState, } from 'react';
-import { Formik } from 'formik';
+import React, { FC, Fragment, useEffect, useState } from "react";
+import { Formik } from "formik";
 
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
-import { fetchData } from '../util/get-data';
-import { ClassroomInputField } from '../components/form/classroom-input-field';
+import {
+  fetchData,
+  validateClasses,
+  findClassesByClassroom
+} from "../util/get-data";
+import { ClassroomInputField } from "../components/form/classroom-input-field";
 
+import classes from "../asset/202010.test.json";
 
 const HomeScreen: FC = () => {
-    const [allClasses, setAllClasses] = useState();
-    const [filteredClasses, setFilteredClasses] = useState();
+  const [allClasses, setAllClasses] = useState({});
+  const [filteredTimes, setFilteredTimes] = useState({});
 
-    // useEffect(() => {
-    //     setAllClasses(fetchData)
-    // }, []);
+  useEffect(() => {
+    // const getData = async () => {
+    //   setAllClasses(await fetchData());
+    // };
+    setAllClasses(validateClasses(classes));
+  }, []);
 
-    return (
-        <Box textAlign="center" alignSelf="center">
-            <Typography variant="h3">
-                Search for a classroom!
-           </Typography>
-            <Formik
-                initialValues={{ classroom: '' }}
-                onSubmit={(value) => console.log(value)}
-                render={props => <ClassroomInputField {...props} />}
-            />
-            
-
-        </Box>
-    )
-}
+  return (
+    <Box textAlign="center" alignSelf="center">
+      <Typography variant="h3">Search for a classroom!</Typography>
+      <Formik
+        initialValues={{ classroom: "" }}
+        onSubmit={value => {
+          setFilteredTimes(findClassesByClassroom(allClasses, value.classroom));
+          console.log(filteredTimes);
+        }}
+        render={props => <ClassroomInputField {...props} />}
+      />
+    </Box>
+  );
+};
 
 export { HomeScreen };
--- src/util/get-data.ts
+++ src/util/get-data.ts
@@ -1,46 +1,79 @@
-import axios from 'axios';
+import axios from "axios";
 
-const fetchData = () => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
+const fetchData = async () => {
+  const date = new Date();
+  const year = date.getFullYear() + 1;
+  const month = date.getMonth();
 
-    const semesterKey = getSemesterKey(month);
-    
-    const url ='https://searchneu.com/data/v2/getTermDump/neu.edu/' + year + semesterKey
-    const data = axios.get(url);
+  const semesterKey = getSemesterKey(month);
+  const url =
+    "https://searchneu.com/data/v2/getTermDump/neu.edu/" +
+    year +
+    semesterKey +
+    ".json";
 
-    if (!data) return {};
+  const data = await axios.get(url, {
+    headers: {
+      "Access-Control-Allow-Origin": "true",
+      "Content-Type": "application/json"
+    }
+  });
 
-    const classes = validateClasses(data)
+  if (data || data === {}) return {};
 
-    return classes;
-}
+  const classes = validateClasses(data);
+
+  return classes;
+};
 
 // Using the current month, return the correct semester key
 const getSemesterKey = (month: number) => {
-    if (month >= 0 && month <= 4) {
-        return 30;
-    } else if (month > 4 && month <= 6) {
-        return 40;
-    } else if (month > 6 && month <= 8) {
-        return 60;
-    } else if (month > 8 && month <= 11) {
-        return 10;
-    } else {
-        return 0;
-    }
-}
+  if (month >= 0 && month < 4) {
+    return 30;
+  } else if (month >= 4 && month < 6) {
+    return 40;
+  } else if (month >= 6 && month < 8) {
+    return 60;
+  } else if (month >= 8 && month <= 11) {
+    return 10;
+  } else {
+    return 0;
+  }
+};
 
 // Remove classes that do not need a classroom
 const validateClasses = (data: any) => {
-    let classes: any = {};
-    Object.keys(data["sectionMap"]).forEach( key => {
-        if (!data[key]["online"]) {
-            classes[key] = data[key]
-        }
-    })
-    return classes;
-}
+  let classes: any = {};
+  Object.keys(data["sectionMap"]).forEach(key => {
+    if (!data["sectionMap"][key]["online"]) {
+      classes[key] = data["sectionMap"][key];
+    }
+  });
+  return classes;
+};
 
-export { fetchData }
+// Given a classroom, find the classes
+const findClassesByClassroom = (allClasses: any, classroom: string) => {
+  let validClasses: any = {};
+  Object.keys(allClasses).forEach(singleClass => {
+    Object.keys(allClasses[singleClass]["meetings"]).forEach((meeting: any) => {
+      if (allClasses[singleClass]["meetings"][meeting]["where"] === classroom) {
+        Object.keys(
+          allClasses[singleClass]["meetings"][meeting]["times"]
+        ).forEach(dayOfWeek => {
+          if (validClasses[dayOfWeek]) {
+            validClasses[dayOfWeek].concat(
+              allClasses[singleClass]["meetings"][meeting]["times"][dayOfWeek]
+            );
+          } else {
+            validClasses[dayOfWeek] =
+              allClasses[singleClass]["meetings"][meeting]["times"][dayOfWeek];
+          }
+        });
+      }
+    });
+  });
+  return validClasses;
+};
+
+export { findClassesByClassroom, fetchData, validateClasses };
